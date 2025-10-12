ca8775bc6dc54a6dff83cb4a70a8b0ec682c2d27
Arom32 <jhhyun2005@naver.com>
2025-10-12 13:55:14 +0900

    feat: 댓글기능과 DB 연결

--- sns/comments/models.py
+++ sns/comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model): #새 테이블
+    content = models.CharField(max_length=200) #col 생성
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # max_length 데이터 최대길이, 
+    # auto_now_add=True 데이터 생성 시, 생성 시각 저장.
+
+    def __str__(self):
+        return f"{self.content[:20]}..." 
+    # Commemt 객체 출력시의 출력 값 
+    # 보통 admin 페이지에서의 데이터 보여줌 + logging debug 용도
--- sns/comments/views.py
+++ sns/comments/views.py
@@ -1,21 +1,16 @@
-# from django.shortcuts import render
+from django.shortcuts import render, redirect #w3_redirect 
+from .models import Comment 
+#w3 _ models.py에서 Commet Model 가져옴.
+# . models, .
 
-# def comment_view(request):
-#     comment = None #초기값 
-
-#     if( request.method == 'POST'): 
-#         comment = request.POST.get('comment_text') 
-
-#     return render(request, 'comments/comment.html', {'comment':comment})
-#             #
-# # Create your views here.
-
-from django.shortcuts import render
-
-def comment_view(request):  
-    comment = None  
-    
+def comment_view(request):
     if request.method == 'POST': 
-        comment = request.POST.get('comment_text') 
-    
-    return render(request, 'comments/comment.html', {'comment': comment})
+        content = request.POST.get('content') 
+        Comment.objects.create(content=content) #w3 테이블에 새 행을 생성 + 내용 추가
+        return redirect('comments:comment') #w3 urls.py app_name:path_name 
+
+    comments = Comment.objects.all().order_by('-created_at') #w3 목록표시
+    # Comment.objects.all() 모든 데이터 가져오기
+    #.order_by('-created_at') 내림차순으로 정렬 
+    #cf) created_at 오름차순
+    return render(request, 'comments/comment.html', {'comments': comments})
