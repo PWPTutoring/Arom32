db7d5708cf02d5e8d1e2d31e74b093762ce668a8
Arom32 <jhhyun2005@naver.com>
2025-11-09 13:29:50 +0900

    feat: 폼과 static files

--- sns/comments/urls.py
+++ sns/comments/urls.py
@@ -5,4 +5,5 @@ app_name = 'comments' #앱 이름
 
 urlpatterns = [
     path('', views.comment_view, name='comment'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- sns/comments/views.py
+++ sns/comments/views.py
@@ -1,16 +1,54 @@
-from django.shortcuts import render, redirect #w3_redirect 
-from .models import Comment 
-#w3 _ models.py에서 Commet Model 가져옴.
-# . models, .
+from django.shortcuts import render, redirect
+from .models import Comment
+from .forms import CommentForm, SignUpForm
 
 def comment_view(request):
     if request.method == 'POST': 
-        content = request.POST.get('content') 
-        Comment.objects.create(content=content) #w3 테이블에 새 행을 생성 + 내용 추가
-        return redirect('comments:comment') #w3 urls.py app_name:path_name 
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('comments:comment')
+    else:
+        form = CommentForm()
 
-    comments = Comment.objects.all().order_by('-created_at') #w3 목록표시
-    # Comment.objects.all() 모든 데이터 가져오기
-    #.order_by('-created_at') 내림차순으로 정렬 
-    #cf) created_at 오름차순
-    return render(request, 'comments/comment.html', {'comments': comments})
+    comments = Comment.objects.all().order_by('-created_at')
+    return render(request, 'comments/comment.html', {
+        'form': form,
+        'comments': comments
+    })
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('comments:comment')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'comments/signup.html', {'form': form})
+
+
+
+
+
+# from django.shortcuts import render, redirect #w3_redirect 
+# from .models import Comment 
+# #w3 _ models.py에서 Commet Model 가져옴.
+# # . models, .
+
+# def comment_view(request):
+#     if request.method == 'POST': 
+#         content = request.POST.get('content') 
+#         Comment.objects.create(content=content) #w3 테이블에 새 행을 생성 + 내용 추가
+#         return redirect('comments:comment') #w3 urls.py app_name:path_name 
+
+#     comments = Comment.objects.all().order_by('-created_at') #w3 목록표시
+#     # Comment.objects.all() 모든 데이터 가져오기
+#     #.order_by('-created_at') 내림차순으로 정렬 
+#     #cf) created_at 오름차순
+#     return render(request, 'comments/comment.html', {'comments': comments})
+
+
